c1fe4bd0367d7c99c3d5f16964ffcd1346cddc69
ruka_charo <ruka.and.little.charo1@gmail.com>
2021-01-05 18:35:22 +0900

    Create_day1

---
+++ alien_invasion/game.py
@@ -0,0 +1,28 @@
+import sys
+import  pygame
+
+class AlienInvasion:
+    '''ゲームのアセットと動作を管理する全体的なクラス'''
+
+    def __init__(self):
+        '''ゲームを初期化し、ゲームのリソースを作成する。'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('エイリアン侵略')
+
+    def run_game():
+        '''ゲームのメインループを開始する。'''
+        while True:
+            #キーボードとマウスのイベントを監視する。
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #最新の状態の画面を表示する。
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #ゲームのインスタンスを作成し、ゲームを実行する。
+    ai = AlienInvasion()
+    ai.run_game()
