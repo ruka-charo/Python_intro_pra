3aaa7bfe0ce0c6ca9abe561b143b27c64b85a0f4
ruka_charo <ruka.and.little.charo1@gmail.com>
2021-01-09 19:09:28 +0900

    Create_day5

--- alien_invasion/game.py
+++ alien_invasion/game.py
@@ -2,6 +2,7 @@ import sys
 import  pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     '''ゲームのアセットと動作を管理する全体的なクラス'''
@@ -21,6 +22,7 @@ class AlienInvasion:
         pygame.display.set_caption('エイリアン侵略')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -28,6 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
 
@@ -41,14 +44,18 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+
     def _check_keydown_events(self, event):
         '''キーを押すイベントに対応する。'''
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
+
 
     def _check_keyup_events(self, event):
         '''キーを離すイベントに対応する。'''
@@ -57,10 +64,31 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+
+    def _fire_bullet(self):
+        '''新しい弾を生成し、Bulletsグループに追加する。'''
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+
+    def _update_bullets(self):
+        '''弾の位置を更新し、古い弾を廃棄する。'''
+        #弾の位置を更新する。
+        self.bullets.update()
+
+        #見えなくなった弾を廃棄する。
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                
+
     def _update_screen(self):
         '''画面上の画像を更新し、新しい画面に切り替える。'''
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
