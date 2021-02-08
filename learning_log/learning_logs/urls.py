d0a812ef60648950b88b6b661ece242f64fc5499
ruka-charo <ruka.and.little.charo1@gmail.com>
2021-02-08 18:56:39 +0900

    Create3_day6

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
         path('topics/', views.topics, name='topics'),
         #個別のトピックの詳細ページ
         path('topics/<int:topic_id>/', views.topic, name='topic'),
+        #新規トピックの追加ページ
+        path('new_topic/', views.new_topic, name='new_topic'),
+        #新規記事の追加ページ
+        path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -20,3 +21,41 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    '''新規トピックを追加する。'''
+    if request.method != 'POST':
+        #データは送信されていないのでからのフォームを生成する。
+        form = TopicForm()
+    else:
+        #POSTでデータが送信されたのでこれを処理する。
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    #空または向こうのフォームを表示する。
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    '''特定のトピックに新規記事を追加する。'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #データは送信されていないので空のフォームを生成する。
+        form = EntryForm()
+    else:
+        #POSTでデータが送信されたのでこれを処理する。
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    #空または無効のフォームを表示する
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
